a8b3f4b48cf95bb87e1ae92be3b1c4fe96a78a94
alejocampos1 <alee.1894@gmail.com>
2025-01-10 06:53:32 -0600

    20250110 - Fixes

--- app.py
+++ app.py
@@ -46,4 +46,4 @@ def upload_file():
     return f"Archivo '{file.filename}' subido exitosamente", 200
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=8080)
